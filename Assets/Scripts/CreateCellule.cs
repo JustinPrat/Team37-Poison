ca51c7c150afbc75f45fe34fc6c35329b362cdc9
Margot <margotdelage02@gmail.com>
2025-09-03 12:05:28 +0200

    [ADD] create cellule

---
+++ Assets/Scripts/CreateCellule.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CreateCellule : MonoBehaviour
+{
+    [SerializeField] Cellule cellulePrefab;
+
+    [SerializeField] private Cellule _cellule;
+
+    private void Start()
+    {
+        _cellule = Instantiate(cellulePrefab);
+    }
+
+    public void ReadSquareInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log(_cellule.baseForm);
+            if (_cellule.baseForm == Base.None)
+            {
+                Debug.Log("Square");
+                _cellule.baseForm = Base.Square;
+            }
+            else
+            {
+                Debug.Log("Naaan");
+            }
+        }
+    }
+
+    public void ReadCircleInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log(_cellule.baseForm);
+            if (_cellule.baseForm == Base.None)
+            {
+                _cellule.baseForm = Base.Circle;
+            }
+            else
+            {
+                Debug.Log("Naaan");
+            }
+        }
+    }
+
+    public void ReadTriangleInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log(_cellule.baseForm);
+            if (_cellule.baseForm == Base.None)
+            {
+                _cellule.baseForm = Base.Triangle;
+            }
+            else
+            {
+                Debug.Log("Naaan");
+            }
+        }
+    }
+
+    public void ReadArrowUpInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _cellule.arrowUp = true;
+        }
+    }
+
+    public void ReadArrowDownInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _cellule.arrowDown = true;
+        }
+    }
+
+    public void ReadArrowRightInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _cellule.arrowRight = true;
+        }
+    }
+
+    public void ReadValidateInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //Pour valider
+            _cellule = Instantiate(cellulePrefab);
+        }
+    }
+}
--- Assets/Scripts/Grille.cs
+++ Assets/Scripts/Grille.cs
@@ -1,17 +1,31 @@
+using Alchemy.Inspector;
 using UnityEngine;
 
 public class Grille : MonoBehaviour
 {
     [SerializeField] Cellule cellulePrefab;
 
+    [Button]
     private void InitGrille(int x, int y)
     {
+        DestroyGrille();
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < y; j++)
             {
-                Instantiate(cellulePrefab);
+                Cellule cellule = Instantiate(cellulePrefab, this.transform);
+                cellule.transform.position = new Vector3(i, j, 0);
             }
+        }
+    }
+
+    private void DestroyGrille()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            #if UNITY_EDITOR
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            #endif
         }
     }
 }
